0cca5c9bd4ce1b2b19414c9757cca80e3baa8611
Moli <facundo1052@gmail.com>
2022-10-04 15:23:09 -0300

    change title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Workshop DevOps - Hospital Britanico' });
+  res.render('index', { title: 'Workshop DevOps - Mimen' });
 });
 
 module.exports = router;
